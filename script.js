58d9c4dd459a92ad86bd28c10d68770bf8cbc230
Viren Patel <p.1023viren@gmail.com>
2023-12-15 01:12:38 +0530

    Added basic css and made calculator functional using javascript

---
+++ script.js
@@ -0,0 +1,62 @@
+function displays(output) {
+    document.getElementById("display_output").innerHTML = output;
+}
+
+let n = "";
+let historyToggle = -1;
+let bracketToggle = -1;
+
+function clearResult() {
+    n = "";
+    bracketToggle = -1;
+    return displays(n);
+}
+function clearLast() {
+    n = n.slice(0, -1);
+    return displays(n);
+}
+function calculate(value) {
+    n = n.concat(value);
+    return displays(n);
+}
+
+function isOperator(char) {
+    return ['+', '-', '*', '/', '%'].includes(char);
+}
+
+function result() {
+    try {
+        var ans = eval(n);
+        return displays(n);
+
+        // Add to history
+       addToHistory(n + ' = ' + ans);
+    } catch (error) {
+        return displays("Error");
+    }
+}
+
+
+
+
+function addToHistory(item) {
+    var historyList = document.getElementById('historyList');
+    var listItem = document.createElement('li');
+    listItem.textContent = item;
+    historyList.appendChild(listItem);
+}
+function showHistory() {
+    bracketToggle = bracketToggle * (-1);
+    if(bracketToggle > 0)
+        document.getElementById('test').style.display = "unset";
+    else if(bracketToggle < 0)
+    document.getElementById('test').style.display = "none";
+}
+function addBracket() {
+    bracketToggle = bracketToggle * (-1);
+    if(bracketToggle > 0)
+        n = n.concat("(");
+    else if(bracketToggle < 0)
+    n = n.concat(")");
+    return displays(n);
+}
